7221f58ed8333a83c552daf639931050c350fd4f
mfrantilla <mfrantilla@eagles.ewu.edu>
2014-11-16 00:30:06 -0800

    Getting Slider to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,14 @@
 $(document).ready( function() {
-    var slider = $.fn.fsvs({
-        speed : 500,
-        bodyID : 'fsvs-body',
-        selector : '> .slide',
-        mouseSwipeDisance : 40,
-        afterSlide : function(){},
-        beforeSlide : function(){},
-        endSlide : function(){},
-        mouseWheelEvents : true,
-        mouseWheelDelay : false,
-        mouseDragEvents : true,
-        touchEvents : true,
-        arrowKeyEvents : true,
-        pagination : true,
-        nthClasses : false,
-        detectHash : true
-   
-    });
-   
-    //slider.slideUp();
-    //slider.slideDown();
-    //slider.slideToIndex( index );
+
+
+	slidr.create('slidr-home', {
+        breadcrumbs: true,
+        controls: 'corner',
+        fade: false,
+        keyboard: true,
+        overflow: true,
+        touch: true,
+        transition: 'cube'
+    }).start();
 
 });
